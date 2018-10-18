1d1f3ecc928747dfa210b69ef06c956dfff69446
douglasorend <37510276+douglasorend@users.noreply.github.com>
2018-10-17 21:36:08 -0500

    v2.0 - November 14th, 2015
    v2.0 - November 14th, 2015
    o Changed all spoiler tags to block-level tags.
    o Changed bbcode tag types so that a validation function can be used to parse content inside tags.
    o Changed the [b][nobbc][spoiler=text]{content}[/spoiler][/nobbc][/b] form to not parse other block-level tags.
    o Moved all language modifications to the end of the language file.

--- Subs-BBCode-OffTopic.php
+++ Subs-BBCode-OffTopic.php
@@ -16,17 +16,47 @@ function BBCode_OffTopic(&$bbc)
 {
 	global $txt;
 
+	// Define these tags, which have no issue with block-level bbcode tags:
 	$bbc[] = array(
 		'tag' => 'offtopic',
-		'before' => '<div style="padding: 3px; font-size: 1em;"><div style="text-transform: uppercase; border-bottom: 1px solid #5873B0; margin-bottom: 3px; font-size: 0.8em; font-weight: bold; display: block;"><span onClick="if (this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display != \'\') {  this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'\'; this.innerHTML = \'<b>' . $txt['offtopic'] . ': </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $txt["debug_hide"] . '</a>\'; } else { this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'none\'; this.innerHTML = \'<b>' . $txt['offtopic'] . ': </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $txt["debug_show"] . '</a>\'; }" /><b>' . $txt['offtopic'] . ': </b><a href="#" onClick="return false;">' . $txt["debug_show"] . '</a></span></div><div class="quotecontent"><div style="display: none;">',
-		'after' => '</div></div></div>',
+		'type' => 'unparsed_content',
+		'parameters' => array(
+			'text' => array('optional' => true),
+			'quote' => array('optional' => true, 'quoted' => true),
+		),
+		'content' => '<div style="padding: 3px; font-size: 1em;"><div style="text-transform: uppercase; border-bottom: 1px solid #5873B0; margin-bottom: 3px; font-size: 0.8em; font-weight: bold; display: block;"><span onClick="if (this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display != \'\') {  this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'\'; this.innerHTML = \'<b>>{text}{quote}: </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $txt["debug_hide"] . '</a>\'; } else { this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'none\'; this.innerHTML = \'<b>{text}{quote}: </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $txt["debug_show"] . '</a>\'; }" /><b>{text}{quote}: </b><a href="#" onClick="return false;">' . $txt["debug_show"] . '</a></span></div><div class="quotecontent"><div style="display: none;">$1</div></div></div>',
+		'validate' => 'BBCode_OffTopic_Validate',
+		'block-level' => true,
 	);
+	$bbc[] = array(
+		'tag' => 'offtopic',
+		'type' => 'unparsed_content',
+		'content' => '<div style="padding: 3px; font-size: 1em;"><div style="text-transform: uppercase; border-bottom: 1px solid #5873B0; margin-bottom: 3px; font-size: 0.8em; font-weight: bold; display: block;"><span onClick="if (this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display != \'\') {  this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'\'; this.innerHTML = \'<b>' . $txt['offtopic'] . ': </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $txt["debug_hide"] . '</a>\'; } else { this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'none\'; this.innerHTML = \'<b>' . $txt['offtopic'] . ': </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $txt["debug_show"] . '</a>\'; }" /><b>' . $txt['offtopic'] . ': </b><a href="#" onClick="return false;">' . $txt["debug_show"] . '</a></span></div><div class="quotecontent"><div style="display: none;">$1</div></div></div>',
+		'validate' => 'BBCode_OffTopic_Validate',
+		'block-level' => true,
+	);
+
+	// Gather up all the block-level bbcode tags, then include that list in the next tag definition:
+	$disallowed = array();
+	foreach ($bbc as $code)
+		if (!empty($code['block_level']))
+			$disallowed[] = $code['tag'];
 	$bbc[] = array(
 		'tag' => 'offtopic',
 		'type' => 'parsed_equals',
 		'before' => '<div style="padding: 3px; font-size: 1em;"><div style="text-transform: uppercase; border-bottom: 1px solid #5873B0; margin-bottom: 3px; font-size: 0.8em; font-weight: bold; display: block;"><span onClick="if (this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display != \'\') {  this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'\'; this.innerHTML = \'<b>' . $txt['offtopic'] . ': $1 &#149; </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $txt["debug_hide"] . '</a>\'; } else { this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'none\'; this.innerHTML = \'<b>' . $txt['offtopic'] . ': $1 &#149; </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $txt["debug_show"] . '</a>\'; }" /><b>' . $txt['offtopic'] . ': $1 &#149; </b><a href="#" onClick="return false;">' . $txt["debug_show"] . '</a></span></div><div class="quotecontent"><div style="display: none;">',
 		'after' => '</div></div></div>',
+		'disallow_children' => array_unique($disallowed),
+		'block-level' => true,
 	);
+}
+
+function BBCode_OffTopic_Validate(&$tag, &$data, &$disabled)
+{
+	if (empty($data))
+		$tag['content'] = '';
+	else
+		$data = parse_bbc($data);
 }
 
 function BBCode_OffTopic_Button(&$buttons)
@@ -37,8 +67,8 @@ function BBCode_OffTopic_Button(&$buttons)
 		'image' => 'offtopic',
 		'code' => 'offtopic',
 		'description' => $txt['offtopic'],
-		'before' => '[offtopic]',
-		'after' => '[/offtopic]',
+		'before' => '[spoiler]',
+		'after' => '[/spoiler]',
 	);
 }
 
--- readme.txt
+++ readme.txt
@@ -1,15 +1,25 @@
 [hr]
-[center][color=red][size=16pt][b]EXPANDING & COLLAPSING OFFTOPIC TAG v1.8[/b][/size][/color]
-[url=http://www.simplemachines.org/community/index.php?action=profile;u=253913][b]By Dougiefresh[/b][/url] -> [url=http://custom.simplemachines.org/mods/index.php?mod=3990]Link to Mod[/url]
+[center][color=red][size=16pt][b]EXPANDING & COLLAPSING OFFTOPIC TAG v2.0[/b][/size][/color]
+[url=http://www.simplemachines.org/community/index.php?action=profile;u=253913][b]By Dougiefresh[/b][/url] -> [url=http://custom.simplemachines.org/mods/index.php?mod=3981]Link to Mod[/url]
 [/center]
 [hr]
 
 [color=blue][b][size=12pt][u]Introduction[/u][/size][/b][/color]
-This modification adds a BBCode to the forum that allows the post author to hide off-topic messages in the post, which is clickable to display it to the user.
+This modification adds a BBCode to the forum that allows the post author to hide a offtopic in the post, which is clickable to display it to the user.
 
 This BBCode takes the form:
-[code][nobbc][offtopic=Some Text]{content goes here}[/offtopic]
-[offtopic]{content goes here}[/offtopic][/nobbc][/code]
+[code][nobbc]
+[offtopic]{content}[/offtopic]
+[offtopic text=Blah]{content}[/offtopic]
+[offtopic quote="Blah"]{content}[/offtopic]
+[/nobbc][/code]
+Note that quotes are required for the [b]quote[/b] parameter, but not for the [b]text[/b] parameter.  These forms have no issues with other block-level tags inside the offtopic tag.
+
+This BBCode can also take the following form:
+[code][nobbc]
+[offtopic=Some Text]{content goes here}[/offtopic]
+[/nobbc][/code]
+Note that, for this form, other block-level bbcode tags are not parsed inside this [b]offtopic[/b] tags form.
 
 [color=blue][b][size=12pt][u]Admin Settings[/u][/size][/b][/color]
 This BBCode may be disabled by going into the [b]Admin[/b] => [b]Forum[/b] => [b]Posts and Topics[/b] => [b]Bulletin Board Code[/b] and unchecking the bbcodes you don't want to use.  You may also be uninstall this mod in order to disable it.
@@ -18,7 +28,7 @@ This BBCode may be disabled by going into the [b]Admin[/b] => [b]Forum[/b] => [b
 This mod was tested on SMF 2.0.9, but should work on SMF 2.1 Beta 1, as well as SMF 2.0 and up.  SMF 1.x is not and will not be supported.
 
 [color=blue][b][size=12pt][u]Changelog[/u][/size][/b][/color]
-The changelog has been removed and can be seen at [url=http://www.xptsp.com/board/index.php?topic=110.msg149#msg149]XPtsp.com[/url].
+The changelog has been removed and can be seen at [url=http://www.xptsp.com/board/index.php?topic=49.msg150#msg150]XPtsp.com[/url].
 
 [color=blue][b][size=12pt][u]License[/u][/size][/b][/color]
 Copyright (c) 2015, Douglas Orend
