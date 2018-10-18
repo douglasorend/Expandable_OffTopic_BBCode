5f81bb7bb4e1102e2c4f4526250f164a03aba749
douglasorend <37510276+douglasorend@users.noreply.github.com>
2018-10-17 21:35:16 -0500

    v1.8 - July 20th, 2015
    v1.8 - July 20th, 2015
    o Fixed missing BBCode button in editor for SMF 2.1 Beta 1.
    o Remade Off-topic bbcode button with different font.

--- Subs-BBCode-OffTopic.php
+++ Subs-BBCode-OffTopic.php
@@ -2,11 +2,15 @@
 /**********************************************************************************
 * Subs-BBCode-OffTopic.php
 ***********************************************************************************
+* This mod is licensed under the 2-clause BSD License, which can be found here:
+*	http://opensource.org/licenses/BSD-2-Clause
 ***********************************************************************************
 * This program is distributed in the hope that it is and will be useful, but      *
 * WITHOUT ANY WARRANTIES; without even any implied warranty of MERCHANTABILITY    *
 * or FITNESS FOR A PARTICULAR PURPOSE.                                            *
 **********************************************************************************/
+if (!defined('SMF'))
+	die('Hacking attempt...');
 
 function BBCode_OffTopic(&$bbc)
 {
